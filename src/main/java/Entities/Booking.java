37cf2cc45a758625bc699bdf9b7a6b86829aee52
Luana Stela <luana.arantes.lsa@gmail.com>
2023-01-09 18:05:03 -0300

    Atualização.

--- src/main/java/Entities/Booking.java
+++ src/main/java/Entities/Booking.java
@@ -16,51 +16,39 @@ public class Booking {
         this.bookingdates = bookingDates;
         this.additionalneeds = additionalNeeds;
     }
-
     public float getTotalprice() {
         return totalprice;
     }
-
     public void setTotalprice(float totalprice) {
         this.totalprice = totalprice;
     }
-
     public boolean getDepositpaid() {
         return depositpaid;
     }
-
     public void setDepositpaid(boolean depositpaid) {
         this.depositpaid = depositpaid;
     }
-
     public String getAdditionalneeds() {
         return additionalneeds;
     }
-
     public void setAdditionalneeds(String additionalneeds) {
         this.additionalneeds = additionalneeds;
     }
-
     public BookingDates getBookingdates() {
         return bookingdates;
     }
-
     public void setBookingdates(BookingDates bookingdates) {
         this.bookingdates = bookingdates;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
--- src/main/java/Entities/BookingDates.java
+++ src/main/java/Entities/BookingDates.java
@@ -12,15 +12,12 @@ public class BookingDates {
     public String getCheckin() {
         return checkin;
     }
-
     public void setCheckin(String checkin) {
         this.checkin = checkin;
     }
-
     public String getCheckout() {
         return checkout;
     }
-
     public void setCheckout(String checkout) {
         this.checkout = checkout;
     }
